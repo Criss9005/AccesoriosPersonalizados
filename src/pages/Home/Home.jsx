46fb5a50b539a71c3abbfbbf70a6a71a093ebc3f
Criss9005 <cdjpn5@gmail.com>
2024-04-14 17:44:28 -0500

    json config

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ export default function Home() {
   
   return (
     <div style={{paddingLeft:40}}>    
-          <h2>Bienvenido a la aplicacion de Registro de contactos telefonicos</h2>
+          
       <img src="https://ayudaleyprotecciondatos.es/wp-content/uploads/2020/09/desactivar-datos-moviles-00.jpg" width='800px' alt="fonde de contactos" />
         
     </div>
