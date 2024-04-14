d83c257556fc990b102ab43b5b4c3cc532f3e71f
Criss9005 <cdjpn5@gmail.com>
2024-04-14 17:45:17 -0500

    json config2

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div style={{paddingLeft:40}}>    
           
-      <img src="https://ayudaleyprotecciondatos.es/wp-content/uploads/2020/09/desactivar-datos-moviles-00.jpg" width='800px' alt="fonde de contactos" />
+    <h1>hola</h1>
         
     </div>
   )
